ce3b837b6ae01ce7ab87f9ba932896a1e1ee5847
Elvis Josue <elvisjosue98@hotmail.com>
2024-10-05 12:08:18 -0600

    cambiando la peticion

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // UTILIZAR CUANDO ESTE EN PRODUCCIÓN
-  baseURL: "https://www.enviamgs.ideasdistintas.net:5000/api",
+  baseURL: "https://enviamgs.ideasdistintas.net:5000/api",
   // baseURL: "http://localhost:4000/api",
   withCredentials: true,
 });
